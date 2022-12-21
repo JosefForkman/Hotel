4dcbc89dfade22ce8fdf938a76551b510f55b223
JosefForkman <strand.vatten@outlook.com>
2022-12-21 18:39:02 +0100

    add footer & header component

---
+++ include/footer.include.php
@@ -0,0 +1,24 @@
+<footer>
+    <div class="Öppetider">
+        <h2 class="text-Amaranth-Purplen">Öppetider</h2>
+        <ul>
+            <li class="text-Gunmetal">Mon Fre: 8.00 - 22.00</li>
+            <li class="text-Gunmetal">Lör Sön: 8.00 - 18.00</li>
+        </ul>
+    </div>
+
+    <div class="Sosiala">
+        <a href="#"><i class="fa-brands fa-square-facebook text-Amaranth-Purplen"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram text-Amaranth-Purplen"></i></a>
+        <a href="#"><i class="fa-brands fa-square-twitter text-Amaranth-Purplen"></i></a>
+        <a href="#"><i class="fa-brands fa-discord text-Amaranth-Purplen"></i></a>
+    </div>
+
+    <div class="Hotel">
+        <h2 class="text-Amaranth-Purplen">Hotel</h2>
+        <ul>
+            <li><a class="text-Gunmetal" href="tel:+46727008222">072-700 82 22</a></li>
+            <li><a class="text-Gunmetal" href="mailto:josfor0501@skola.goteborg.se">josfor0501@skola.goteborg.se</a></li>
+        </ul>
+    </div>
+</footer>
---
+++ include/header.include.php
@@ -0,0 +1,15 @@
+<header>
+    <div class="hamburger bg-Russian-Green" id="hamburgerOpen"></div>
+    <nav>
+        <ul>
+            <li><a class="text-Amaranth-Purplen" href="">Hem</a></li>
+            <li><a class="text-Amaranth-Purplen" href="">Spa</a></li>
+            <li><a class="text-Amaranth-Purplen" href="">Restaurang</a></li>
+            <li><a class="text-Amaranth-Purplen" href="">Om oss</a></li>
+            <li><a class="text-Amaranth-Purplen" href="">Kontakta</a></li>
+        </ul>
+    </nav>
+
+    <button class="btn bg-Amaranth-Purplen text-Snow">Boka</button>
+
+</header>
