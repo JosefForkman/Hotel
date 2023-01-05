ac877396566935621a87ce9a253a5e8dc608587e
JosefForkman <strand.vatten@outlook.com>
2023-01-05 19:00:15 +0100

    add island function in yrgopelago api

--- API/classes/yrgopelago/api.php
+++ API/classes/yrgopelago/api.php
@@ -64,6 +64,10 @@
 
             return false;
         }
+        public function island(): array {
+            $islands = $this->client->request('GET', 'islands');
+            return json_decode($islands->getBody()->getContents(), true);
+        }
 
         private function isValidUuid(string $uuid): bool {
             if (!is_string($uuid) || (preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/', $uuid) !== 1)) {
