d8a2a3c55cfe5e71a583b3adc594748ef35f08ba
JosefForkman <strand.vatten@outlook.com>
2023-01-06 20:10:06 +0100

    update nav btn

--- include/header.include.php
+++ include/header.include.php
@@ -2,7 +2,7 @@
     <div class="hamburger bg-Russian-Green" id="hamburgerOpen"></div>
     <nav>
         <ul>
-            <li><a class="text-Amaranth-Purplen" href="">Hem</a></li>
+            <li><a class="text-Amaranth-Purplen" href="/">Hem</a></li>
             <li><a class="text-Amaranth-Purplen" href="">Spa</a></li>
             <li><a class="text-Amaranth-Purplen" href="">Restaurang</a></li>
             <li><a class="text-Amaranth-Purplen" href="">Om oss</a></li>
@@ -10,6 +10,6 @@
         </ul>
     </nav>
 
-    <button class="btn bg-Amaranth-Purplen text-Snow">Boka</button>
+    <a href="boende.php" class="btn bg-Amaranth-Purplen text-Snow">Hitta Rum</a>
 
 </header>
